7590e655d377d365813faf6c5dc32487eb2b7ed4
Priyanka <priyankashah3107@gmail.com>
2024-04-06 01:42:14 +0530

    IMG add

---
+++ frontend/src/assets/index.js
@@ -0,0 +1,16 @@
+import Ameri from "./Ameri.png";
+import cappaci from "./cappaci.png";
+import conte from './conte.png';
+import content from './content.png';
+import Espresso from "./Espresso.png";
+import latOne from "./latOne.png";
+import latTwo from "./latTwo.png";
+import latThree from "./latThree.png";
+import Macca from './Macca.png';
+import Main from "./Main.png";
+import Product from "./Product.png";
+import Time from "./Time.png";
+import history from './history.png';
+import Vanilla from "./Vanilla.png"
+import IcedLat from './IcedLat.png';
+
