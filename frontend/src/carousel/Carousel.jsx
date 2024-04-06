bd71738b1c7bbf5c63df0d4eb4b162afed6dccbb
Priyanka <priyankashah3107@gmail.com>
2024-04-07 01:46:30 +0530

    fix icons

--- frontend/src/carousel/Carousel.jsx
+++ frontend/src/carousel/Carousel.jsx
@@ -6,18 +6,24 @@ import { FaChevronRight } from "react-icons/fa6";
 function Carousel({ data }) {
   // console.log(data);
   return (
-    <div className='carousel '>
-     <FaChevronLeft  className='text-white arrow arrow-left '/>
+    <div className='car '>
 
       {data.map((slide) => (
+        
         <div key={slide.id} className="carousel-item">
+          
+     
           <img src={slide.src} alt={slide.alt} className='slide' />
-          <div className="carousel-content">
+          <div className="carousel-content items-center flex  flex-col justify-between">
+          <FaChevronLeft  className='text-white arrow arrow-left -ml-96 '/>
             <h2>{slide.head}</h2>
             <p>{slide.desc}</p>
+            
             <button>Learn More</button>
+            <FaChevronRight  className='arrow arrow-right -mr-96'/>
+           
           </div>
-          <FaChevronRight  className='arrow arrow-right'/>
+          {/* <FaChevronRight  className='arrow arrow-right'/> */}
           
           <span className='indicators'>
         {data.map((_, idx) => (
