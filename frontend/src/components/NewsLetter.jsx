18cc3bf60319fe485770d274ec8f5fb35c6b392c
Priyanka <priyankashah3107@gmail.com>
2024-04-08 12:17:51 +0530

    z-index

--- frontend/src/components/NewsLetter.jsx
+++ frontend/src/components/NewsLetter.jsx
@@ -3,22 +3,11 @@ import React from 'react'
 function NewsLetter() {
   return (
      <> 
-     <div className="text-white">
-            Thank you Universe for giving me this life and everything I don't know  <br />
-            what the end of the life is matter... but i am try to finding my path so always give me a light to <br />
-            see the things clearly also i am at this point of stage where i am thiking what to i choose <br />
-            for living but nothing is clear don't knnow what is happening everyday, everyday i wake up <br />
-            with lots of engery.........  but when i am dying then what's matter how i live what i do in my life <br />
-            sikhne ko bhot kuch h life mei universe aur tumene time hi itna kam diya h kya kru kaise jiyun <br />
-            jindgi tumne di h  death bhi tum hi doge maine to kuch nhi manga tha phir kyun life k existence mei <br />
-            mei aane se phle mei ky kuch thi nhi ya death bhi baad kuch houngi ya nhi agar nahi to bas ye bacche hue 50 saal <br />
-            mei kya kru iss time ka batao tumhi mei ko confuse hoti ja rhi hu existence ho janane ki koshish mei <br />
-            kabhi kabhi ye puri existence sab jhut sa lgta hai kabhi sach sa to kaise decide kru <br />
-            bas marne se phle sikka dena jo kuch bhi iss life se sikhne ko hai aur ek baar universe marne <br />
-            se phle mere confusion bhi khtm kr dena mere saath saath bas aur kuch bhi nhi aur puri duniya bhi mil jaye <br />
-            to kya to milga kuch bhi to nhi to tum hi milna universe milna ho to bas death se phle bas aur baki sab chiz k liye 
-            dhyanwaad aur prem. 
-     </div>
+    <div class="absolute ml-10 flex h-[full] w-screen flex-col">
+  <div class="relative top-24 -z-10 mt-20 h-72 w-[600px] border-2 border-stone-500">
+    <div class="relative z-40 ml-10 mt-8 h-72 w-[600px] border-2 border-stone-500">elemensts</div>
+  </div>
+</div>
      </>
   )
 }
