8e2bfc3a36c3aa760447d241e77a47eb135367d6
Priyanka <priyankashah3107@gmail.com>
2024-04-15 19:55:20 +0530

    SignIn Auth

--- frontend/src/context/FirebaseAuth.jsx
+++ frontend/src/context/FirebaseAuth.jsx
@@ -1,6 +1,6 @@
 import { useContext,createContext  } from "react";
 import {app} from '../../firebase'
-import {getAuth,  createUserWithEmailAndPassword   } from 'firebase/auth'
+import {getAuth,  createUserWithEmailAndPassword, signInWithEmailAndPassword  } from 'firebase/auth'
 
 const FirebaseContext = createContext(null); 
 
@@ -14,11 +14,15 @@ export const FirebaseContextProvider = ({ children }) => {
        return createUserWithEmailAndPassword(auth, email, password)
   }
 
+  const SignInWithEmail = (email, password) => {
+      return signInWithEmailAndPassword(auth, email, password)
+  }
+
 
   return (
     <>
       
-      <FirebaseContext.Provider value={{createUserWithEmail}}>
+      <FirebaseContext.Provider value={{createUserWithEmail, SignInWithEmail}}>
         {children}
       </FirebaseContext.Provider>
     </>
